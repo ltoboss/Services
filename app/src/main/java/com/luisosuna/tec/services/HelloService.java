e2938858dc629aca1a89b2154590ca3e3e1644c2
ltoboss <Alumno@utmzap.tecmilenio.mx>
2018-10-25 20:47:13 -0500

    Descomponiendo

--- app/src/main/java/com/luisosuna/tec/services/HelloService.java
+++ app/src/main/java/com/luisosuna/tec/services/HelloService.java
@@ -2,13 +2,18 @@ package com.luisosuna.tec.services;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.IBinder;
 import android.util.Log;
+
+import java.net.URL;
 
 public class HelloService extends Service {
 
     private String serviceTag = "HELLO_SERVICE";
+
     private Boolean isRunning = false;
+    private String asyncTag = "ASYNC_TAG";
 
 
     public HelloService() {
@@ -56,5 +61,33 @@ public class HelloService extends Service {
     public IBinder onBind(Intent intent) {
         // TODO: Return the communication channel to the service.
         throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    public class DownloaderAsync extends AsyncTask<URL, Integer, String>{
+
+        @Override
+        protected String doInBackground(URL... urls) {
+            Log.i(asyncTag, "doInBackground");
+            return null;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            Log.i(asyncTag, "onPreExecute");
+            super.onPreExecute();
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            Log.i(asyncTag, "onPostExecute");
+            super.onPostExecute(s);
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            Log.i(asyncTag, "onProgressExecute");
+            Log.i(asyncTag, values.toString());
+            super.onProgressUpdate(values);
+        }
     }
 }
